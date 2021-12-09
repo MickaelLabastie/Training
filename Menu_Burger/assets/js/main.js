94cf2207c948af7d1fbfa26951d6752874bdc682
Mickaelux <mickaelux@gmail.com>
2021-12-09 23:22:16 +0100

    New tests on menu burger

---
+++ Menu_Burger/assets/js/main.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', function() {
+    var spin = document.querySelector('.spin-container');
+    spin.classList.toggle('active');
+})
